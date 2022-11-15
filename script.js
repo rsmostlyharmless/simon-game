40b3b57c14761c3dd32189895ef9bd24a9d1202a
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-11-15 17:53:55 +0100

    random color select working

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+`use strict`;
 
+const buttonColors = [`red`, `blue`, `green`, `yellow`];
+const gamePattern = [];
+
+const nextSequence = function () {
+    const randomNum = Math.trunc(Math.random() * 4);
+    const randomCol = buttonColors[randomNum];
+    gamePattern.push[randomCol];
+};
+nextSequence();
